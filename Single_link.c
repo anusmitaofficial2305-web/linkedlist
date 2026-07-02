a254c691e98c4afa4f477f4742023cbf511cc656
Anusmita Das <anusmita.official.2006@gmail.com>
2026-07-02 16:19:53 +0530

    "version 1"

---
+++ Single_link.c
@@ -0,0 +1,203 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int choice;
+    while(1) {
+        printf("\n1.Insert Begin\n2.Insert Last\n3.Any Insert\n4.Delete begin\n5.Delete Last\n6.Delete All\n7.Any delete\n8.Display\n9.EXIT\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1: beginsert(); break;
+            case 2: lastInsert(); break;
+            case 3: anyinsert(); break;
+            case 4: begdelete(); break;
+            case 5: lastdelete(); break;
+            case 6: alldelete(); break;
+            case 7: anydelete(); break;
+            case 8: display(); break;
+            case 9: exit(0);
+            default: printf("Invalid choice.\n");
+        }
+    }
+    return 0;
+}
+
+typedef struct node{
+    int data;
+    int value;
+    struct node *link;
+}nd;
+
+nd *head;
+
+nd *createNode(int value){
+    nd *ptr = (nd*)malloc(sizeof(nd));
+    ptr->data = value;    
+    ptr->link = NULL;
+    return ptr;
+}
+
+void display(){
+    nd *temp = head;
+    if(temp == NULL){
+        printf(" Empty\n");
+    }else{
+        printf("\nStart->");
+        while(temp!=NULL){
+            printf("%d->", temp->data);
+            temp = temp->link;
+        }
+        printf("End\n\n");
+    }
+}
+
+void beginsert(){
+    int element;
+    printf("Enter the value: ");
+    scanf("%d", &element);
+
+    nd* newnode = createNode(element);
+
+    newnode->link = head;
+    head = newnode;
+    printf("Element inserted successfully\n\n");
+}
+
+void lastInsert(){
+    int element;
+    printf("Enter the value: ");
+    scanf("%d", &element);
+
+    nd* newnode = createNode(element);
+
+    nd *temp = head;
+    while(temp->link!=NULL){
+        temp=temp->link;
+    }
+
+    temp->link = newnode;
+    newnode->link = NULL;
+    printf("Element inserted successfully\n\n");
+}
+
+void anyinsert(){
+    int pos=0, element, i, counter=1;
+    printf("Enter the position to insert = ");
+    scanf("%d", &pos);
+
+    nd *temp;
+    temp = head;
+    while(temp->link != NULL){
+       counter = counter + 1;
+       temp = temp->link;
+    }
+
+    temp = head;
+    if(pos > counter || pos <= 0){
+        printf("Sorry, position is not valid!\n");
+    } else {
+        printf("Enter the value: ");
+        scanf("%d", &element);
+        nd *newNode = createNode(element);
+
+        temp = head;
+        for(i = 0; i < pos - 2; i++){
+            temp = temp->link;
+        }
+
+        newNode->link = temp->link;
+        temp->link = newNode;  
+        printf("Element inserted successfully\n\n");
+    }
+}
+
+
+void begdelete(){
+    int element;
+    nd *temp;
+
+    if(head == NULL){
+        printf("Empty\n");
+    } else {
+        element = head->data;
+        temp = head;
+        head = head->link;
+
+        free(temp);
+        printf("Element deleted {%d} successfully\n\n", element);
+    }
+}
+
+void lastdelete(){
+    int element, i, counter = 1;
+    nd *temp, *prev;
+    temp = head;
+
+    if(head == NULL){
+        printf("Empty\n");
+    } else {
+        while(temp->link != NULL){
+            counter++;
+            temp = temp->link;
+        }
+
+        if(counter == 1){
+            begdelete();
+            return;
+        }
+
+        temp = head;
+        for(i = 0; i < counter - 2; i++){ 
+            temp = temp->link;
+        }
+
+        prev = temp->link;
+        element = prev->data;
+        temp->link = NULL;
+
+        free(prev);
+        printf("Element deleted {%d} successfully\n\n", element);
+    }
+}
+
+void anydelete(){
+    int i, pos = 0, counter = 1;
+    nd* temp = head;
+
+    printf("Enter the position to delete = ");
+    scanf("%d", &pos);
+
+    while(temp->link != NULL){
+        counter++;
+        temp = temp->link;
+    }
+
+    temp = head;
+    if(pos > counter || pos <= 0){
+        printf("Sorry, position is not valid!\n");
+    } else {
+        if(pos == 1){
+            begdelete();
+        } else {
+            for(i = 0; i < pos - 2; i++){ 
+                temp = temp->link;
+            }
+            
+            nd* ptr = temp->link;        
+            temp->link = ptr->link;   
+
+            free(ptr);                  
+            printf("Element deleted successfully\n\n");
+        }
+    }
+}
+
+void alldelete(){
+    nd *temp = head;
+    while(head != NULL){
+        lastdelete();
+    }
+    printf("All Element deleted successfully\n\n");
+    printf("\tNow run \033[0;32mrm -rf\033[0m on sys32 dirctory\n\n");
+}
